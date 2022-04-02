cfbd70681d7a91b285b5c2841e266d171927cbc6
JaneJung-K <piaojane@daum.net>
2022-04-02 09:07:00 +0900

    Initial Commit

---
+++ StoryBoardTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoardTutorial
+//
+//  Created by Leah on 2022/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
